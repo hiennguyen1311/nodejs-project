af482db2d346430d1eecbe6b443d44dd350bb237
HDS\hphucnguyen <hien.phucnguyen@hitachids.com>
2024-05-20 15:45:57 +0700

    Fix

--- src/services/router/user.router.ts
+++ src/services/router/user.router.ts
@@ -6,11 +6,7 @@ const UserRouter = express.Router();
 
 UserRouter.get("/api/v1/profile", async (req, res) => {
 	if (checkAuthenTication(req, res, true)) {
-		if (req.headers.authorization) {
-			return apiResult(res, true, true, null);
-		} else {
-			return apiResult(res, false, undefined, "Missing Authorization");
-		}
+		return apiResult(res, true, true, null);
 	}
 
 	return;
